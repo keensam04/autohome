1265eb70afc5051b54d382f98d69530de8b3c037
misdaque <misdaque.azdee960@gmail.com>
2020-04-09 12:12:28 +0530

    Created HealthCheck api...

---
+++ src/main/java/com/autohome/Main.java
@@ -0,0 +1,13 @@
+package com.autohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("This is spring app");
+    }
+}
---
+++ src/main/java/com/autohome/com/autohome/controller/HealthCheck.java
@@ -0,0 +1,14 @@
+package com.autohome.com.autohome.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @RequestMapping(value = "/ping" ,method = RequestMethod.GET)
+    public String showHealthCheck(){
+        return "Pong";
+    }
+}
