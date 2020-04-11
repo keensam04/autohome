a553df65530fad5e15f77a59e97db82dd9b7e38f
misdaque <misdaque.azdee960@gmai.com>
2020-04-11 21:53:52 +0530

    Removed Environment variable and added target folder in .gitignore

--- src/main/java/com/autohome/controller/HealthCheck.java
+++ src/main/java/com/autohome/controller/HealthCheck.java
@@ -1,16 +1,11 @@
 package com.autohome.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheck {
-
-    @Autowired
-    private Environment env;
 
     @RequestMapping(value = "/ping" ,method = RequestMethod.GET)
     public String showHealthCheck(){
