dfad51e14505803c8f5996f5344f38b167738362
misdaque <misdaque.azdee960@gmail.com>
2020-04-11 13:28:29 +0530

    Logs

--- src/main/java/com/autohome/controller/HealthCheck.java
+++ src/main/java/com/autohome/controller/HealthCheck.java
@@ -1,5 +1,7 @@
 package com.autohome.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
+    Logger log = LoggerFactory.getLogger(HealthCheck.class);
+
     @RequestMapping(value = "/ping" ,method = RequestMethod.GET)
     public String showHealthCheck(){
+        log.info("Logs here" );
         return "Pong";
     }
 }
