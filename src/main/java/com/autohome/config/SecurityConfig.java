def3ae248ef82841c2bf8eaf554b24d28a602aeb
misdaque <misdaque.azdee960@gmai.com>
2020-04-30 20:29:03 +0530

    Authentication_03

--- src/main/java/com/autohome/config/SecurityConfig.java
+++ src/main/java/com/autohome/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.autohome.config;
 
-import com.autohome.service.CustomUserOidcService;
+import com.autohome.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,19 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomUserOidcService userOidcService;
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-       /* http.authorizeRequests()
-                .antMatchers("/","/ping","/login","/error").permitAll()
-                .anyRequest().authenticated().and().userDetailsService();*/
 
         http
                 .authorizeRequests().antMatchers("/ping","/ping/*", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login().userInfoEndpoint().oidcUserService(userOidcService);
+                .oauth2Login().userInfoEndpoint().oidcUserService(userService);
     }
 }
